b2865cbbebd6cdf934904d16e81d61337d6df628
millarba <millarba@gmail.com>
2017-02-16 18:48:32 -0600

    fixed schedule logic

--- models/schedule.rb
+++ models/schedule.rb
@@ -13,38 +13,38 @@ class Schedule
     until divisible_runtime % 5 == 0
       divisible_runtime += 1
     end
-  p  divisible_runtime
+      divisible_runtime
   end
 
   def possible_showings
-    @showings = 0
+    @possible_showings = 0
     remaining_minutes = total_weekday_minutes
     while remaining_minutes >= @runtime
       remaining_minutes -= 15
+      
         if remaining_minutes >= divisible_runtime
-          p remaining_minutes
+          
           remaining_minutes -= divisible_runtime
-          @showings += 1
-        end
-        if remaining_minutes >= 0 
+          
+          @possible_showings += 1
+
           remaining_minutes -= 20
         end
     end
-    p @showings
+    p "There are #{@possible_showings} showings"
   end
 
   def showings
     today = Time.now
-    number_of_showings = @showings
+    number_of_showings = @possible_showings
 
     open = Time.new(today.year, today.month, today.day, 11, 15)
 
     showings = []
     while number_of_showings > 0
-      p number_of_showings
-      showings << open
-      open = open + (60 * 160)
       number_of_showings -= 1
+      showings << open
+      open = open + (60 * (divisible_runtime + 20 + 15))
     end
     showings
   end
@@ -52,7 +52,7 @@ class Schedule
 
 end
 
-lego_batman = Schedule.new(114)
+lego_batman = Schedule.new(132)
 
 lego_batman.possible_showings
 
